047211c1e8540b3ef78d168c4144b251ff1c1633
InnerAc <anjicun@live.com>
2017-05-23 13:14:56 +0800

    增加了获取某兼职下报名的用户，也需要小白加一下service

--- src/main/java/com/partjob/controller/MchntPageController.java
+++ src/main/java/com/partjob/controller/MchntPageController.java
@@ -1,8 +1,16 @@
 package com.partjob.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.partjob.model.JobInfo;
+import com.partjob.model.UserInfo;
+import com.partjob.service.JobService;
 
 /**
  * Created by InnerAc on 17/5/15.
@@ -12,8 +20,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "mchntp")
 public class MchntPageController {
 	
+	@Autowired
+	JobService jobService;
+	
 	@RequestMapping("{page}")
 	public String merto(@PathVariable String page){
 		return "mchnt/"+page;
+	}
+	
+	@RequestMapping("info/{jid}")
+	public String jobinfo(@PathVariable int jid,Model model){
+		JobInfo job = jobService.getById(jid);
+		model.addAttribute("job", job);
+		return "mchnt/info";
+		
+	}
+	/**
+	 * 获取某个兼职报名的人员列表
+	 * @param jid
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("peoplelem/{jid}")
+	public String peoplelem(@PathVariable int jid,Model model){
+		model.addAttribute("users",new ArrayList<UserInfo>());	//这里要包含用户id，用户姓名，用户评分
+		return "mchnt/peoplelem";
 	}
 }
