5a1ee2fea16211b3c6a0c95169373efdada586de
InnerAc <anjicun@live.com>
2017-06-02 17:58:44 +0800

    修改了用户与兼职之间的关系常量代码
    修改了用户与兼职之间的关系常量代码

--- src/main/java/com/partjob/constant/CommonCanstant.java
+++ src/main/java/com/partjob/constant/CommonCanstant.java
@@ -43,9 +43,9 @@ public class CommonCanstant {
 	
 	//****************************用户兼职常量******************/
 	public static int USER_PASS=1; //报名通过
-	public static int USER_NOT_PASS=0;//报名不通过
-	public static int USER_ABSENCE=2;//工作缺勤
-	public static int USER_LEAVE_HALFWAY=3;//工作中途离开
+	public static int USER_NOT_JOIN=0;//未报名
+	public static int USER_REFUSE=2;//拒绝报名
+	public static int USER_ABSENCE=3;//工作缺勤
 	public static int USER_WORK_FULL=4;//工作满勤
 	public static int USER_WORK_CHECK_MONRY=200;//用户参加兼职保证金
 	
